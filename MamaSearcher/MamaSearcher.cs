8fae04455c91129dce675a387ddc3475b78c8291
Yakov Pizenberg <thelittlecitizen17@gmail.com>
2020-08-23 20:40:38 +0300

    Add the final solutaion

---
+++ MamaSearcher/MamaSearcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MamaSearcher
+{
+    class MamaSearcher : IMamaSearcher
+    {
+        public Dictionary<string,Action<int,string>> Stock { get; set; }
+
+        public MamaSearcher(Dictionary<string, Action<int, string>> stock)
+        {
+            Stock = stock;
+        }
+        public void PerformSearch(string content)
+        {
+
+            int index;
+
+            foreach(var v in Stock)
+            {
+                if (content.Contains(v.Key))
+                {
+                    index = content.IndexOf(v.Key);
+                    v.Value(index,v.Key);
+                    
+
+;               }
+            }
+
+        }
+
+        public void Subscribe(string pattern, Action<int, string> actionToPerform)
+        {
+            Stock.Add(pattern, actionToPerform);
+        }
+
+    }
+}
---
+++ MamaSearcher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MamaSearcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int, string> FirstAction = Output;
+            Action<int, string> SecondAction = MagicFound;
+
+            Dictionary<string, Action<int,string>> stock = new Dictionary<string, Action<int,string>>() { { "yasha", SecondAction },{ "b", FirstAction } };
+
+            MamaSearcher mamaSearcher = new MamaSearcher(stock);
+
+            mamaSearcher.PerformSearch("my name is yasha");
+            mamaSearcher.PerformSearch("my name is bob");
+
+        }
+        public static void Output(int index, string pattern)
+        {
+            Console.WriteLine($"Pattern {pattern} found at index {index}");
+        }
+        public static void MagicFound(int index, string pattern)
+        {
+            Console.WriteLine($"{pattern} WAS FOUND !!!! KULULULU");
+        }
+
+    }
+}
